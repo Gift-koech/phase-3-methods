2f0076391cd64897090520689fab475c56b0b313
Gift-koech <koechgift93@gmail.com>
2022-10-17 14:27:29 +0300

    completed the lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
-# Your code here!
+def greet_programmer
+    puts " Hello, programmer" 
+end
+
+def greet(name = "JID")
+    puts "Hello #{name}"
+end
+
+def greet_with_default(name= "")
+    puts "Hi there,#{name}"
+end
+
+greet_with_default "sam"
+
+def add 
+    return 1 + 2
+end
+
+def halve(4)
+    return 4/2
+end
